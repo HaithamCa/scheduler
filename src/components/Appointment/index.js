3be196c045e34e6feda74aa916e8d890931a809e
hytham defrawy <hythamdefrawy@gmail.com>
2021-12-10 20:26:43 -0500

    return Header and Show and Empty component from index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 const Appointment = (props) => {
-    return (
-        <article className="appointment">
-             {props.time}
-        </article>
-    );
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </article>
+  );
 };
 
 export default Appointment;
